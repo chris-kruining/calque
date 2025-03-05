ee8006f834fd21f87d5cd96f04d2033148fcf29a
Chris Kruining <c.kruining@4dotnet.nl>
2025-03-05 16:34:17 +0100

    idk anymore. yolo

--- src/features/theme/context.tsx
+++ src/features/theme/context.tsx
@@ -18,6 +18,8 @@ export interface State {
 const getSession = async () => {
     'use server';
 
+    console.log(process.env.SESSION_SECRET);
+
     return useSession<State>({
         password: process.env.SESSION_SECRET!,
     });
